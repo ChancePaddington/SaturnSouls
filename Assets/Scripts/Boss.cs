80b98c82dcc8f02fc82cce0f45fa0fa200b3cd9d
ChancePaddington <aaronpatrickhopkins@gmail.com>
2025-05-06 16:52:53 +0100

    Fixed shield. Used corotine to spawn minions. Added minion waypoints. Minion moving to waypoint but not stopping.

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    private GameObject player;
+
     //Rocket variables
     [SerializeField] private GameObject rocketPrefab;
     [SerializeField] private Transform firingPoint;
@@ -11,22 +13,45 @@ public class Boss : MonoBehaviour
     [SerializeField] private float fireRate = 0.5f;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float lifeTime = 4f;
-    private float timer = 2f;
+    private float laserTimer = 2f;
+
+    //Minion variables
+    [SerializeField] Transform minionSpawn;
+    [SerializeField] Transform minionPrefab;
+    private float minionTimer = 10f;
 
     private void Start()
     {
-        StartCoroutine(Shoot(timer));
-        Debug.Log("Coroutine Check");
+        //Contols the amount of time between laser shots
+        StartCoroutine(Shoot(laserTimer));
+        //Controls the amount of time between minion spawns
+        StartCoroutine(Minion(minionTimer));
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     private IEnumerator Shoot(float timePeriod)
     {
         while (true)
         {
+            //Spawns rocket
             Instantiate(rocketPrefab, firingPoint.position, firingPoint.rotation);
             Debug.Log("1212");
+            //Suspends the coroutine for set amount of seconds
             yield return new WaitForSeconds(timePeriod);
         }
     } 
+
+    private IEnumerator Minion(float timePeriod)
+    {
+        while (true)
+        {
+            //Spawns minion
+            Instantiate(minionPrefab, minionSpawn.position, minionSpawn.rotation);
+            yield return new WaitForSeconds(timePeriod);
+        }
+    }
+
 
 }
--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -1,16 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Minion : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Range(1, 10)]
+    [SerializeField] private float speed = 10f;
+
+    private GameObject waypoint;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float force = 1.0f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.up * speed;
+        //Finds the game object with the tag to move to
+        waypoint = GameObject.FindGameObjectWithTag("Minion Waypoint");
+
+        Vector3 direction = waypoint.transform.position - transform.position;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+
+    }
+    //public void SetMovement(int x, int y)
+    //{
+    //    SetMovement(new Vector2(x, y)); 
+    //}
+
+    //private void StopMovement()
+    //{
+    //   SetMovement(new Vector2(0,0));
+    //}
+
+
+    private void Update()
+    {
+        //Get the distance between the minion and the waypoint
+
+        //If this is within a threshold (e.g. 0.1f)
+
+        //We have arrived! So set the linear velocity to zero
+
+
+
+
+        //if (transform.position == waypoint.transform.position)
+        //{
+        //    StopMovement();
+        //    Debug.Log("StopeMovement in IF");
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
