48915e9c8fcffd528b371758533cf42792359b0d
ChancePaddington <aaronpatrickhopkins@gmail.com>
2025-05-08 16:50:47 +0100

    End of day May 8th, added enemy and player health scripts

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public Image healthIcon;
+    public int maxHealth = 10;
+    private int currentHealth;
+
+    private void Start()
+    {
+      
+    }
+
+}
